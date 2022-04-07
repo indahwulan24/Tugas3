62a0cbde4617a88e1a72687b54cb03ffa23470cc
indahwulan24 <12050120353@students.uin-suska.ac.id>
2022-04-07 14:19:23 +0700

    1

--- app/src/main/java/com/example/goapps/activity/LoginActivity.java
+++ app/src/main/java/com/example/goapps/activity/LoginActivity.java
@@ -2,6 +2,9 @@ package com.example.goapps.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -41,6 +44,22 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    //handle pengubahan bahasa
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId()==R.id.action_settings) {
+            Intent nIntent = new Intent(Settings.ACTION_LOCALE_SETTINGS);
+            startActivity(nIntent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void login_sukses() {
